dfad494cf3198b9f0c447c5bace0c10680c92fcd
Jeff <jolson615@gmail.com>
2021-02-02 12:11:09 -0500

    Update planner.py

--- planner.py
+++ planner.py
@@ -2,6 +2,7 @@
 
 ## 1. Define a function `perfect_day` that returns the string, “Time for fun in the sun!” if the weather is sunny. 
 ##    If it’s rainy, return the string, “Enjoy a book and some tea indoors.”
+##    (This function has two outcomes, so it requires two tests, which have been written already in the `test_planner.py` file)
 
 def perfect_day(weather):
     return
@@ -10,12 +11,14 @@ def perfect_day(weather):
 ##    If the weather is cloudy, it should suggest taking a trip to the botanic garden.
 ##    If the weather is windy, it should suggest flying a kite at the park.
 ##    If the weather is sunny, it should suggest going for a bike ride.
+##    (Requires 3 tests, they haven't been written yet)
 
 ## 3. Temperatures tend to fluctuate in your city. Write a function that takes in two arguments: weather, and temperature. 
 ##    Like the previous function, it should recommend an activity based on the weather conditions. 
 ##    Additionally, it should suggest bringing a coat if the temperature is below 50 degrees. 
 ##    If the temperature is below 40 degrees, it should suggest wearing a coat, hat and scarf. 
 ##    If the temperature is above 50 degrees it should only suggest an activity.
+##    (Requires at least 3 tests)
 
 ## 4. Sometimes you like to spend the day indoors, regardless of the weather. Write a function that takes in two arguments: weather, and indoor or outdoor preference.
 ##    If weather is sunny and preference is outdoors, suggest going for a bike ride.
@@ -24,5 +27,6 @@ def perfect_day(weather):
 ##    If weather is windy and preference is outdoor, suggesting getting crafty and making a weather vane.
 ##    If weather is cloudy and preference is outdoors, suggest visiting the botanic garden.
 ##    If weather is cloudy and the preference is indoors, suggest cozying up with your favorite book.
+##    (Requires at least 6 tests)
 
 ## 5. [STRETCH] Challenge yourself to add more suggestions based on other weather conditions that we haven't covered yet. If the user enters a weather condition that you don't recognize, return a error. 
